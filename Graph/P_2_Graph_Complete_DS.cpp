fe286ae9aa3047080b8744c0667510fa97345bf2
abhishekhandacse <abhishekhandacse@gmail.com>
2018-08-03 15:20:59 +0530

    Added DFS module

--- Graph/P_2_Graph_Complete_DS.cpp
+++ Graph/P_2_Graph_Complete_DS.cpp
@@ -7,19 +7,39 @@ using namespace std;
 class Graph{
 	int V;
 	vector<vector<int>> G;
-
+	// Auxillary function for DFS
+	void DFS_util(vector<bool> &visited,int S);
 public:
 	Graph(int V){
 		this->V=V;
-		// G(V);
-		G.resize(20);
+		G.resize(V);
 	}
-	void addEdge(int u,int v){
-		G[u].push_back(v);
-		// G[v].push_back(u);
-	}
+	void addEdge(int u,int v);
 	void BFS(int S);
+	void DFS(int S);
+	
 };
+void Graph::addEdge(int u,int v){
+	G[u].push_back(v);
+}
+// Functions for doing DFS
+
+void Graph::DFS_util(vector<bool> &visited,int S){
+	visited[S]=true;
+	cout<<S<<" ";
+	for(auto i:G[S]){
+		if(visited[i]==false)
+			DFS_util(visited,i);
+	}
+}
+void Graph::DFS(int S){
+	vector<bool> visited(V,false);
+
+	DFS_util(visited,S);
+	cout<<endl;
+}
+// Function for doing BFS
+
 void Graph::BFS(int S){
 	vector<bool> visited(V,false);
 	list<int> Q;
@@ -42,6 +62,7 @@ void Graph::BFS(int S){
 			
 		
 	}
+	cout<<endl;
 }
 int main()
 {
@@ -56,7 +77,7 @@ int main()
  
     cout << "Following is Breadth First Traversal "
          << "(starting from vertex 2) \n";
-    g.BFS(2);
- 
+    // g.BFS(2);
+ 	g.DFS(2);
     return 0;
 }
