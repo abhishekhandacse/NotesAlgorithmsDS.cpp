d15b8cddbde4d01cdf6f0538535e714f0350920f
abhishekhandacse <abhishekhandacse@gmail.com>
2018-08-03 20:13:31 +0530

    Cycle detection in Undirected Graph module added

--- Graph/P_2_Graph_Complete_DS.cpp
+++ Graph/P_2_Graph_Complete_DS.cpp
@@ -1,24 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
-
+// Adjancency list representation of graph
 
 class Graph{
 	int V;
 	vector<vector<int>> G;
 	// Auxillary function for DFS
 	void DFS_util(vector<bool> &visited,int S);
+	// Aux funtion for Counting Paths
+	void CountPathsUtil(int u, int d,vector<bool> &v,int &pathCount);
+	void DFS_checkCycle(int i,bool &ans,vector<bool> &visited);
 public:
-	Graph(int V){
-		this->V=V;
-		G.resize(V);
-	}
+	Graph(int V);
 	void addEdge(int u,int v);
 	void BFS(int S);
 	void DFS(int S);
 	int FindMother();
+	int CountPaths(int S,int D);
+	bool IsCyclic();
 };
+Graph::Graph(int V){
+		this->V=V;
+		G.resize(V);
+}
 void Graph::addEdge(int u,int v){
 	G[u].push_back(v);
 }
@@ -86,19 +91,68 @@ int Graph::FindMother(){
 			return -1;
 	return v;
 }
+// Count all possible paths from source to destination
+/*
+	The idea is to do DFS for the source and use backtracking to calculate the number of paths which lead to destination
+
+*/
+int Graph::CountPaths(int S,int D){
+	vector<bool> visited(V,false);
+	int pathcount=0;
+	CountPathsUtil(S,D,visited,pathcount);
+	return pathcount;
+}
+void Graph::CountPathsUtil(int S,int D,vector<bool> &visited,int &count){
+	visited[S]=true;
+	if(S==D)
+		count++;
+	else{
+		for(auto i:G[S]){
+			if(visited[i]==false)
+				CountPathsUtil(i,D,visited,count);
+		}
+	}
+// Make it false so as to check other paths
+	visited[S]=false;
+}
+
+// Is Graph Cyclic
+/*
+	We can do DFS from every node and check weather some node is begin visited twice
+	If this is the case return true as cycle exists otherwise no cycle exists in the graph
+*/
+bool Graph::IsCyclic(){
+	bool ans=false;
+	vector<bool> visited(V,false);
+	for(int i=0;i<V;i++){
+		if(ans)return ans;
+		DFS_checkCycle(i,ans,visited);
+		fill(visited.begin(),visited.end(),false);
+	}
+	return ans;
+}
+void Graph::DFS_checkCycle(int i,bool &ans,vector<bool> &visited){
+	if(ans)return;
+	visited[i]=true;
+
+	// Do DFS in Adjancecy list of S
+	for(auto ii:G[i]){
+		if(visited[ii]==false)
+			DFS_checkCycle(ii,ans,visited);
+		else {ans=true;break;}
+	}
+}
+
 int main(){
 
-    Graph g(7);
+    Graph g(4);
     g.addEdge(0, 1);
-    g.addEdge(0, 2);
-    g.addEdge(1, 3);
-    g.addEdge(4, 1);
-    g.addEdge(6, 4);
-    g.addEdge(5, 6);
-    g.addEdge(5, 2);
-    g.addEdge(6, 0);
- 
-    cout << "A mother vertex is " << g.FindMother();
- 
+    g.addEdge(1, 2);
+    g.addEdge(2, 3);
+    // g.addEdge(3, 0);
+    
+ 	cout<<g.IsCyclic()<<endl;
     return 0;
+ 
+    
 }
